2903c299fbd8f450f1b3589c73892a5d38dd9e64
Aditi126 <adiime126@gmail.com>
2025-06-29 23:14:07 +0530

    Added skill section to portfolio.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import LandingPage from './components/landingPage/LandingPage'
 import './App.css'
 import About from './components/about/About'
+import SkillSection from './components/skill/SkillSection'
 
 const App = () => {
   return (
     <div className='app-div'>
       <LandingPage />
       <About />
+      <SkillSection />
     </div>
   )
 }
--- src/components/skill/SkillSection.jsx
+++ src/components/skill/SkillSection.jsx
@@ -1,22 +1,36 @@
 import React from 'react'
+import './SkillSection.css';
 
 const SkillSection = () => {
     return (
         <div className='skill-section'>
             <h1>Skills</h1>
-            <div className="Using Now">
-                <h3></h3>
+            <div className="Using-Now">
+                <h3>USING NOW:</h3>
                 <div className="">
-                    <img src="" alt="HTML-logo" />
-                    <img src="" alt="CSS-logo" />
-                    <img src="" alt="JavaScript-logo" />
-                    <img src="" alt="React.js-logo" />
-                    <img src="" alt="Git-logo" />
-                    <img src="" alt="Figma-logo" />
-                    <img src="" alt="Java-logo" />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" alt="HTML-logo" />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" alt="CSS-logo" />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" alt="JavaScript-logo" />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="React.js-logo" />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt="Git-logo" />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" alt="Figma-logo" />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azure/azure-original.svg" alt="Java-logo" />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg" alt="docker-logo" />
                 </div>
             </div>
-            <div className="Using Now">
+            <div className="learning-Now">
+                <h3>LEARNING NOW:</h3>
+                <div className="">
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" alt="" />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" alt="" />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" alt="" />
+                    {/* <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" /> */}
+                </div>
+            </div>
+            {/* <div className="Using Now">
                 <h3></h3>
                 <div className="">
                     <img src="" alt="" />
@@ -27,19 +41,7 @@ const SkillSection = () => {
                     <img src="" alt="" />
                     <img src="" alt="" />
                 </div>
-            </div>
-            <div className="Using Now">
-                <h3></h3>
-                <div className="">
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                </div>
-            </div>
+            </div> */}
         </div>
     )
 }
